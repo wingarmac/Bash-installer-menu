e172a616f3763b1eaec84e0ae7218f2fd30e429b
wingarmac <wingarmac@gmail.com>
2023-01-31 01:06:26 +0100

    Add files via upload

--- installpkgs.sh
+++ installpkgs.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 # Install package list
-for pkg in `cat installation.txt`; do sudo apt-get install -y $pkg; done
+for pkg in `cat installation.txt`; do sudo apt-get install -y $pkg; done;
+echo "Packages installed from installation.txt. Back to Installer-menu after 5 sec.";
+sleep 5;
+bash Installer-menu;
+
--- pkgsexport.sh
+++ pkgsexport.sh
@@ -3,4 +3,7 @@
 # Packages installed with apt install from terminal excl.
 # Output file: installation.txt
 
-awk -F'll ' '  /apt install/ && !/nvidia/ && !/--/ && !/-f/{ print $2 }  ' /var/log/apt/history.log >installation.txt
+awk -F'll ' '  /apt install/ && !/nvidia/ && !/--/ && !/-f/{ print $2 }  ' /var/log/apt/history.log >installation.txt;
+echo "Packages exported to installation.txt. Back to Installer-menu after 5 sec.";
+sleep 5;
+bash Installer-menu;
