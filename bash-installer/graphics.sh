83601bc25217a8b89a6c99e924be86003682f254
Tony, Martin Berbel <wingarmac@gmail.com>
2024-01-18 00:17:12 +0100

    Update graphics.sh

--- bash-installer/graphics.sh
+++ bash-installer/graphics.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
+sudo apt update && sudo apt upgrade -y
+sudo ubuntu-drivers autoinstall -y
 
-# Detect and install NVIDIA drivers
-if ubuntu-drivers devices | grep -q "nvidia"; then
-    sudo ubuntu-drivers autoinstall -y
-else
-    # Check for available upgrades for AMD drivers
-    sudo apt update
-    sudo apt list --upgradable
-    # Insert commands to install AMD drivers if needed
-    # (Please add appropriate commands to install AMD drivers here)
-fi
