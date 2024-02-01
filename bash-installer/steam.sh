d665a82904998e507fcbec79eafc5d478101b30d
Tony, Martin Berbel <wingarmac@gmail.com>
2024-02-01 22:40:19 +0000

    Update steam.sh
    pauses per steps to achieve with title to explain what comes next

--- bash-installer/steam.sh
+++ bash-installer/steam.sh
@@ -1,45 +1,41 @@
 #!/bin/bash
 
-# echo Title: Steam installer
-echo "# echo Title: Steam installer"
+function pause() {
+  read -rp "Press Enter to continue..."
+}
 
-# echo GPG install
-echo "Download with wget steam.gpg from: https://repo.steampowered.com/steam/archive/stable/steam.gpg to: /usr/share/keyrings/steam.gpg"
+echo "Steam installer"
+pause
+
+echo "GPG install"
+pause
 wget -O /usr/share/keyrings/steam.gpg https://repo.steampowered.com/steam/archive/stable/steam.gpg
-echo "# echo Done !"
+echo "Done !"
 
-# echo Repo install
-echo "$ sudo tee /etc/apt/sources.list.d/steam-stable.list <<'EOF'"
-echo "deb [arch=amd64,i386 signed-by=/usr/share/keyrings/steam.gpg] https://repo.steampowered.com/steam/ stable steam"
-echo "deb-src [arch=amd64,i386 signed-by=/usr/share/keyrings/steam.gpg] https://repo.steampowered.com/steam/ stable steam"
-echo "EOF"
+
+echo "Repo install"
+pause
 sudo tee /etc/apt/sources.list.d/steam-stable.list <<'EOF'
 deb [arch=amd64,i386 signed-by=/usr/share/keyrings/steam.gpg] https://repo.steampowered.com/steam/ stable steam
 deb-src [arch=amd64,i386 signed-by=/usr/share/keyrings/steam.gpg] https://repo.steampowered.com/steam/ stable steam
 EOF
-echo "# echo Done !"
+echo "Done !"
 
-# echo add i386 arch and update
-echo "$ sudo dpkg --add-architecture i386"
+
+echo "Add i386 arch and update"
+pause
 sudo dpkg --add-architecture i386
-echo "$ sudo apt-get update"
 sudo apt-get update
-echo "# echo Done !"
+echo "Done !"
 
-# echo Installing Steam Launcher
-echo "$ sudo apt-get install \\"
-echo "  libgl1-mesa-dri:amd64 \\"
-echo "  libgl1-mesa-dri:i386 \\"
-echo "  libgl1-mesa-glx:amd64 \\"
-echo "  libgl1-mesa-glx:i386 \\"
-echo "  steam-launcher"
+
+echo "Installing Steam Launcher"
+pause
 sudo apt-get install \
   libgl1-mesa-dri:amd64 \
   libgl1-mesa-dri:i386 \
   libgl1-mesa-glx:amd64 \
   libgl1-mesa-glx:i386 \
   steam-launcher
-
-# echo done
-echo "# echo done"
-
+echo "Done !"
+pause
