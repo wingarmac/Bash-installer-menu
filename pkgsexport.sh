cb2b5a540282a9f5e490c14d3ed751ed4a29622c
Tony, Martin Berbel <wingarmac@gmail.com>
2024-03-08 09:36:45 +0100

    Update pkgsexport.sh
    compare with dpkg installed now added

--- pkgsexport.sh
+++ pkgsexport.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-# Export manualy installed packages 
-# Packages installed with apt install from terminal excl.
+# Export all installed packages
 # Output file: installation.txt
 
-awk -F'll ' '  /apt install/ && !/nvidia/ && !/--/ && !/-f/{ print $2 }  ' /var/log/apt/history.log >installation.txt;
-echo "Packages exported to installation.txt. Back to Installer-menu after 5 sec.";
-sleep 5;
-bash Installer-menu;
+# Extract manually installed packages from APT history log
+awk -F'll ' '  '/apt install/ && !/nvidia/ && !/--/ && !/-f/{ print $2 }' /var/log/apt/history.log > installation.txt
+
+# Append all installed packages using dpkg
+dpkg -l | grep ^ii | awk '{ print $2 }' >> installation.txt
+
+echo "Packages exported to installation.txt. Back to Installer-menu after 5 sec."
+sleep 5
+bash Installer-menu
