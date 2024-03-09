e63027df9f8c1f4f314ff03f348bd3ae6ea1fc5c
Tony, Martin Berbel <wingarmac@gmail.com>
2024-03-09 23:51:14 +0100

    Update cinnamon.sh
    Adding dialog for Xorg - Wayland selection

--- bash-installer/cinnamon.sh
+++ bash-installer/cinnamon.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-sudo apt install -y xorg slick-greeter lightdm-settings mutter muffin cinnamon yaru-theme-icon oxygen-cursor-theme gnome-software numlockx
+REQUIRED_PKG="dialog"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: "$PKG_OK"
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  apt install -y $REQUIRED_PKG
+fi
+
+# Present the choice using dialog
+choice=$(dialog --title "Choose Display Server" --radiolist "Pick the display server you want to use:" 15 50 2 \
+          "Xorg" "Traditional display server" off \
+          "Wayland" "Modern display server with improved performance and security" off)
+
+case $choice in
+  "Xorg")
+    sudo apt install -y xorg slick-greeter lightdm-settings mutter muffin cinnamon yaru-theme-icon oxygen-cursor-theme gnome-software numlockx
+    ;;
+  "Wayland")
+    sudo apt install -y mutter lightdm-wayland-session yaru-theme-icon oxygen-cursor-theme gnome-software
+    ;;
+  *)
+    echo "Invalid choice."
+    exit 1
+esac
