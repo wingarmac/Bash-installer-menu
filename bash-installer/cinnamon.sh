013dbe62a813338a5a16299155054f0f6b378041
Tony, Martin Berbel <wingarmac@gmail.com>
2024-04-02 01:43:44 +0200

    Update cinnamon.sh
    simple bash upgrade

--- bash-installer/cinnamon.sh
+++ bash-installer/cinnamon.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
-REQUIRED_PKG="dialog"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
-echo Checking for $REQUIRED_PKG: "$PKG_OK"
-if [ "" = "$PKG_OK" ]; then
-  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-  apt install -y $REQUIRED_PKG
-fi
-
-# Present the choice using dialog
-choice=$(dialog --title "Choose Display Server" --radiolist "Pick the display server you want to use:" 15 50 2 \
-          "Xorg" "Traditional display server" off \
-          "Wayland" "Modern display server with improved performance and security" off)
+# Present the choice in a simple text menu
+echo "Choose Display Server:"
+echo "1. Xorg (Traditional display server)"
+echo "2. Wayland (Modern display server with improved performance and security)"
+read -p "Enter your choice (1 or 2): " choice
 
 case $choice in
-  "Xorg")
+  1)
     sudo apt install -y xorg slick-greeter lightdm-settings mutter muffin cinnamon yaru-theme-icon oxygen-cursor-theme gnome-software numlockx
     ;;
-  "Wayland")
+  2)
     sudo apt install -y mutter lightdm-wayland-session yaru-theme-icon oxygen-cursor-theme gnome-software
     ;;
   *)
