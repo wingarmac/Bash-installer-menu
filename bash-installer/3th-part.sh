79ebf9dda03ca4c446308c7b1ae3887d30b5b20d
Tony, Martin Berbel <wingarmac@gmail.com>
2025-07-08 22:37:38 +0200

    Update 3th-part.sh
    Google installation update with additional sub-request for default browser setting

--- bash-installer/3th-part.sh
+++ bash-installer/3th-part.sh
@@ -13,8 +13,29 @@ read -p "Enter your choice: " choice
 case $choice in
     1)
         # Commands to install Google Chrome
-        wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-        sudo dpkg -i google-chrome-stable_current_amd64.deb
+        echo "Installing Google Chrome..."
+        # Download the latest stable Google Chrome .deb package
+        # -q for quiet output from wget
+        wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O google-chrome-stable_current_amd64.deb
+
+        # Install the package and fix broken dependencies in one go
+        # Using -y for automatic yes to prompts
+        # && ensures the next command only runs if the previous one succeeded
+        sudo dpkg -i google-chrome-stable_current_amd64.deb && sudo apt --fix-broken install -y
+
+        # Prompt to set Google Chrome as the default browser
+        read -p "Do you want to set Google Chrome as your default web browser? (y/N): " set_default_choice
+        if [[ "$set_default_choice" =~ ^[Yy]$ ]]; then
+            echo "Setting Google Chrome as the default web browser..."
+            xdg-settings set default-web-browser google-chrome.desktop
+            echo "Google Chrome has been set as your default browser."
+        else
+            echo "Google Chrome will not be set as the default browser."
+        fi
+
+        # Clean up the downloaded .deb file
+        rm -f google-chrome-stable_current_amd64.deb
+        echo "Google Chrome installation complete."
         ;;
     2)
         # Commands to install Webmin
